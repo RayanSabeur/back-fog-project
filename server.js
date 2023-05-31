8ad0cefdec537bac0eb82c0d0ea5a7d9f21531e9
Rayan SABEUR <rayan.sabeur@ozitem.com>
2023-05-31 13:22:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express();
+
+dotenv.config({path: './config/.env'})
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+  }
+  app.use(cors(corsOptions));
+
+  app.listen(process.env.PORT, () => {
+
+    console.log(`listening on port ${process.env.PORT}`)
+})
